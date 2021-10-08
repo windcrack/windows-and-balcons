5130e5fb21e94ffac4fb8c8d02a1b2a8182485e9
Alexander Voevodin <43845313+windcrack@users.noreply.github.com>
2021-10-08 13:28:09 +0300

    new changes on calck

--- script/calc.js
+++ script/calc.js
@@ -1,17 +1,24 @@
 function calc(){
+    // button next
     const nexBtn1 = document.querySelector('.next-btn1');
     const nexBtn2 = document.querySelector('.next-btn2');
     const nexBtn3 = document.querySelector('.next-btn3');
     const nexBtn4 = document.querySelector('.next-btn4');
+    // block steps
     const step1 = document.querySelector('.calck-oject');
     const step2 = document.querySelector('.calck-type-window');
     const step3 = document.querySelector('.calck-dimensions');
     const step4 = document.querySelector('.calck-summ');
     const step5 = document.querySelector('.calck-form');
+    // button back 
+    const back = document.querySelectorAll('.back-bnt')
     const objectValue = document.querySelectorAll('.oject-value');
     const windowValue = document.querySelectorAll('.window-value');
     const dimensionsValue = document.querySelectorAll('.dimensions-value');
     const summValue = document.querySelectorAll('.summ-value');
+    // inner block
+    const totalList = document.querySelector('.block-list-items');
+    let listSelect = [];
 
     function changeBlock(button, blockPrev, blockNext){
         button.addEventListener('click', ()=>{
@@ -25,8 +32,18 @@ function calc(){
             item.addEventListener('input', ()=>{
                 if(item.checked === true){
                     changeBlock(button, blockPrev, blockNext);
+                    listSelect.push(`<li class="item">${item.value}</li>`);
                 }
+                totalList.innerHTML = listSelect.join('');
             })
+            
+        })
+    }
+
+    function backBlock(button, blockNext, blockPrev){
+        button.addEventListener('click', ()=>{
+            blockPrev.style.display = 'flex';
+            blockNext.style.display = 'none';
         })
     }
 
@@ -34,9 +51,9 @@ function calc(){
     checkSelect(windowValue, nexBtn2, step2, step3);
     checkSelect(dimensionsValue, nexBtn3, step3, step4);
     checkSelect(summValue, nexBtn4, step4, step5);
-
-    // changeBlock(nexBtn3, step3, step4);
-    // changeBlock(nexBtn4, step4, step5);
+    back.forEach(item =>{
+        console.log(item);
+    })
 }
 
 
--- script/dist/calc.dev.js
+++ script/dist/calc.dev.js
@@ -6,19 +6,26 @@ Object.defineProperty(exports, "__esModule", {
 exports["default"] = void 0;
 
 function calc() {
+  // button next
   var nexBtn1 = document.querySelector('.next-btn1');
   var nexBtn2 = document.querySelector('.next-btn2');
   var nexBtn3 = document.querySelector('.next-btn3');
-  var nexBtn4 = document.querySelector('.next-btn4');
+  var nexBtn4 = document.querySelector('.next-btn4'); // block steps
+
   var step1 = document.querySelector('.calck-oject');
   var step2 = document.querySelector('.calck-type-window');
   var step3 = document.querySelector('.calck-dimensions');
   var step4 = document.querySelector('.calck-summ');
-  var step5 = document.querySelector('.calck-form');
+  var step5 = document.querySelector('.calck-form'); // button back 
+
+  var back = document.querySelectorAll('.back-bnt');
   var objectValue = document.querySelectorAll('.oject-value');
   var windowValue = document.querySelectorAll('.window-value');
   var dimensionsValue = document.querySelectorAll('.dimensions-value');
-  var summValue = document.querySelectorAll('.summ-value');
+  var summValue = document.querySelectorAll('.summ-value'); // inner block
+
+  var totalList = document.querySelector('.block-list-items');
+  var listSelect = [];
 
   function changeBlock(button, blockPrev, blockNext) {
     button.addEventListener('click', function () {
@@ -32,16 +39,28 @@ function calc() {
       item.addEventListener('input', function () {
         if (item.checked === true) {
           changeBlock(button, blockPrev, blockNext);
+          listSelect.push("<li class=\"item\">".concat(item.value, "</li>"));
         }
+
+        totalList.innerHTML = listSelect.join('');
       });
+    });
+  }
+
+  function backBlock(button, blockNext, blockPrev) {
+    button.addEventListener('click', function () {
+      blockPrev.style.display = 'flex';
+      blockNext.style.display = 'none';
     });
   }
 
   checkSelect(objectValue, nexBtn1, step1, step2);
   checkSelect(windowValue, nexBtn2, step2, step3);
   checkSelect(dimensionsValue, nexBtn3, step3, step4);
-  checkSelect(summValue, nexBtn4, step4, step5); // changeBlock(nexBtn3, step3, step4);
-  // changeBlock(nexBtn4, step4, step5);
+  checkSelect(summValue, nexBtn4, step4, step5);
+  back.forEach(function (item) {
+    console.log(item);
+  });
 }
 
 var _default = calc;
